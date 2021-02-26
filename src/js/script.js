e7c2f0f6fc82d075a850a9d2e29e5dcff47dbca1
Victor <vberkal@inbox.ru>
2021-02-26 19:50:28 +0300

    add tab vith adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,36 @@ $(document).ready(function(){
               }
             }
           ]
-      });
-  });
+    });
 
-  
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content')
+          .removeClass('catalog__content_active').eq($(this).index())
+          .addClass('catalog__content_active');
+    });
+
+    
+
+    function toggleCard(item){
+        $(item).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__front').eq(i).toggleClass('catalog-item__front_active');
+                $('.catalog-item__back').eq(i).toggleClass('catalog-item__back_active');
+            });
+        });
+    }
+
+    toggleCard('.catalog-item__link');
+    toggleCard('.catalog-item__return');
+
+
+    const tab = document.querySelector('.catalog');
+    console.log(tab);
+
+
+});
+
